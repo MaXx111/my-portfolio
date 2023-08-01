77cc5e7f283f0829dd5c9f1eb41f3348c2f96085
Sidorov Maksim <maxx11167@gmail.com>
2023-08-01 18:38:16 +0300

    feat: update about me

--- src/entities/aboutMe.tsx
+++ src/entities/aboutMe.tsx
@@ -1,8 +1,22 @@
+import { data } from "../data/dataProjects"
+
 export const AboutMe: React.FC = () => {
+
+    
 
     return(
         <>
-            About Me
+            <div className="about-me">  
+                <h1 className="about-me_title">Портфолио<br/> Frontend-разработчика</h1>
+                <span>---</span>
+                    <br/>
+                <div className="total_info">
+                    <span className="total">{data.length}</span>
+                    <span className="total_text">Проектов в портфолио</span>
+                    <span className="total_text">Последнее обновление 1 августа 2023 года</span>
+                    <span className="total_text text-end">Подготовлено <a href="" className="total-link">Максимом Сидоровым</a></span>
+                </div>
+            </div>  
         </>
     )
 }
